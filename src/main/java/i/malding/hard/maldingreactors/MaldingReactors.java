90e1a3410392353980eb8eb50be55978c2f48c7c
alpha <alphamodeyt@gmail.com>
2022-04-17 15:10:14 -0500

    Initial commit

---
+++ src/main/java/i/malding/hard/maldingreactors/MaldingReactors.java
@@ -0,0 +1,10 @@
+package i.malding.hard.maldingreactors;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MaldingReactors implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
